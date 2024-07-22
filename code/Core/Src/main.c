3e58ddd65f6390d741b8da957bbccd75a50b9391
imGlam <117605424+imGlam@users.noreply.github.com>
2024-07-22 17:00:42 +0700

    commit from browser

--- code/Core/Src/main.c
+++ code/Core/Src/main.c
@@ -29,7 +29,7 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-// first change
+// change from browser
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
